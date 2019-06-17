eb03c4e68b5de874cd5c5535a7bae364c3b115b6
Mohamed Amin Kedir <m.aminkedir@gmail.com>
2019-06-16 20:38:36 -0500

    host edit

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const db = {}
 const sequelize = new Sequelize("database2", "root", "Amin4012!", {
-    host: "localhost",
+    host: "bluecrew.cq5crrozg6px.us-east-2.rds.amazonaws.com",
     dialect: 'mysql',
 
     pool: {
